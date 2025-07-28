b733702d9f5e24d1a08460c78a8995f1e0ecb7e9
zerui <754376642@qq.com>
2025-07-28 21:07:32 +0800

    20250728

---
+++ temp/programming/packer-driver/targets/editor/chunks/70/7052b35dfbe13ea1766793c74299b451255e5fb1.js
@@ -0,0 +1,12 @@
+System.register([], function (_export, _context) {
+  "use strict";
+
+  return {
+    setters: [],
+    execute: function () {
+      // This module is auto-generated to report error emitted when try to load module file:///Users/zeruili/projects/cocos_project/driftClash/assets/scripts/GameManager.ts at runtime.
+      throw new Error(`Error: 在加载模块文件 /Users/zeruili/projects/cocos_project/driftClash/assets/scripts/GameManager.ts 时发生错误：Error: ENOENT: no such file or directory, open '/Users/zeruili/projects/cocos_project/driftClash/assets/scripts/GameManager.ts'`);
+    }
+  };
+});
+//# sourceMappingURL=7052b35dfbe13ea1766793c74299b451255e5fb1.js.map
